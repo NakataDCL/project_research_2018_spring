21750bd1750738db951bee8a844aa6f37f36a157
Naoki Nakata <>
2018-05-30 17:58:42 +0900

    [update] PUNでの通信機能のデモを実装

---
+++ dev_project_research_2018/Assets/Demos/Scripts/ObjectGenerator.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectGenerator : MonoBehaviour {
+	// Update is called once per frame
+	void Update () {
+		// 生成位置をランダムな座標にする
+        float x = Random.Range(-10f, 10f);
+        float y = Random.Range(-10f, 10f);
+        float z = Random.Range(-10f, 10f);
+        Vector3 pos = new Vector3(x, y, z);
+ 
+        // 左クリックをしたらマッチング環境にCubeのインスタンスを生成する
+        if (Input.GetMouseButtonDown(0)) {
+ 
+            // 第1引数にResourcesフォルダの中にあるプレハブの名前(文字列)
+            // 第2引数にposition
+            // 第3引数にrotation
+            // 第4引数にView ID(指定しない場合は0)
+            GameObject obj = PhotonNetwork.Instantiate("unitychan", pos, Quaternion.identity, 0);
+ 
+            // 生成したオブジェクトに力を加える
+            Rigidbody objRB = obj.GetComponent<Rigidbody>();
+            objRB.AddForce(Vector3.forward * 20f, ForceMode.Impulse);
+        }
+	}
+}
--- dev_project_research_2018/Assets/Demos/Scripts/RandomMatchmaker.cs
+++ dev_project_research_2018/Assets/Demos/Scripts/RandomMatchmaker.cs
@@ -7,24 +7,24 @@ public class RandomMatchmaker : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-		PhotonNetwork.ConnectUsingSettings("0.1");
+		PhotonNetwork.ConnectUsingSettings(null);
 
 		// PhotonVoiceNetwork.Client.DebugEchoMode = false; が存在しない
 		//PhotonVoiceNetwork.Client.SetDebugEchoMode();
 	}
 	
-	// void OnJoinedLobby() が存在しない
+	// ロビーに入ると呼ばれる
 	void OnJoinedLobby(){
+		Debug.Log("ロビーに入りました");
+
+		// ルームに入室する
 		PhotonNetwork.JoinRandomRoom();
 	}
 
-	// void OnPhotonRandomJoinedFailed() が存在しない
-	void OnPhotonRandomJoinedFailed(){
-		PhotonNetwork.CreateRoom(null);
-	}
-
-	// OnJoinedRoom() が存在しない
+	// ルームに入手つすると呼ばれる
 	void OnJoinedRoom(){
+		Debug.Log("ルームへ入室しました。");
+
 		PhotonNetwork.Instantiate(
 			photonObject.name,
 			new Vector3(0.0f, 1.0f, 0.0f),
@@ -33,6 +33,16 @@ public class RandomMatchmaker : MonoBehaviour {
 		);
 		GameObject mainCamera = GameObject.FindWithTag("MainCamera");
 		//mainCamera.GetComponent<ThirdPersonCamera>().enabled = true;
+	}
+
+	// ルームの入室に失敗すると呼ばれる
+	void OnPhotonRandomJoinFailed(){
+		Debug.Log("ルームの入室に失敗しました");
+
+		// ルームがないと入室に失敗するため、その時は自分で作る
+        // 引数でルーム名を指定できる
+		//PhotonNetwork.CreateRoom(null);
+		PhotonNetwork.CreateRoom("myRoomName");
 	}
 
 	void OnGUI(){
