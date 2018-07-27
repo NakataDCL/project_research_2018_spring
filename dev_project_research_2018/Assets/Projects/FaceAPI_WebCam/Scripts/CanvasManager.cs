fbe1e45569e964e73733bd605191d751f3a55efb
Naoki Nakata <>
2018-07-27 15:19:39 +0900

    [update] webカメラから取得した画像を画面上に表示する機能を追加

---
+++ dev_project_research_2018/Assets/Projects/FaceAPI_WebCam/Scripts/CanvasManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CanvasManager : MonoBehaviour {
+	[SerializeField]
+	private RawImage m_displayUI = null;
+
+	private WebCameraManager _WebCamManager = null;
+
+	// Use this for initialization
+	void Start () {
+		_WebCamManager = GameObject.Find("WebCameraManager").GetComponent<WebCameraManager>();
+		if(_WebCamManager == null){
+			Debug.Log("Can't find WebCameraManager.");
+			return;
+		}
+
+		m_displayUI.texture = _WebCamManager.getWebCameraTexture();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		m_displayUI.texture = _WebCamManager.getWebCameraTexture();
+	}
+}
